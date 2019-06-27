ff39a868c22fd4d83471ad59aba7a24c61c15380
Łukasz Nawrat <lnawrat@future-processing.com>
2019-06-27 11:20:36 +0200

    Exercise 0: Added empty pages.

--- fp-app/src/app/app.module.ts
+++ fp-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
