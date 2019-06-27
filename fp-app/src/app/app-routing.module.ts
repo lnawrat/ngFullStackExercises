faaa7a97f42b40b26d4392137e2805bb7259d79b
Łukasz Nawrat <lnawrat@future-processing.com>
2019-06-27 11:26:57 +0200

    Exercise 0: Set up navigation.

--- fp-app/src/app/app-routing.module.ts
+++ fp-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductsComponent } from './pages/products/products.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
